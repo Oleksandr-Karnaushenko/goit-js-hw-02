d1b4b3e17e187c720f0894985a14a939de58154c
Oleksandr <oleksandr.karnaushenko@gmail.com>
2024-03-03 12:37:24 +0100

    hw-2

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function checkForSpam(message) {
+  const regMess = message.toUpperCase(message);
+  if (regMess.includes('SPAM') || regMess.includes('SALE')) {
+    return true;
+  } else return false;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
